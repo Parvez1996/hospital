334d707872edbd932313700a62ebcb3c500135c0
Parvez Pathan <84612665+Parvez1996@users.noreply.github.com>
2024-08-20 22:35:45 +0530

    basic hospital structure

---
+++ api/router.js
@@ -0,0 +1,5 @@
+const router = require("express").Router();
+
+// router.post("/user", user.adduser);
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const bodyparser = require("body-parser");
+require("dotenv").config();
+const cors = require("cors");
+let router = require("./api/router");
+const prisma=require("./api/prismaConnection")
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+app.use(cors());
+async function init() {
+  try {
+    await prisma.$connect()
+    app.use("/", router);
+    app.listen(process.env.PORT, () => {
+      console.log(`Server listening on port ${process.env.PORT}`);
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+init();
